b4cc1a779ca4bf108d8f7b248fa806ba1eb36a22
d0nth4x <contact@pdrzewiecki.pl>
2019-11-16 16:47:43 +0100

    base and main page

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-undef
+import "../css/app.css"
+
+// import "@fortawesome/fontawesome-free"
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "@fortawesome/fontawesome-free/js/all.js"
